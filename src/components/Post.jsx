544a3024e466da92f38e45d0dd04071580c78349
mkdhir15 <mk4904004@gmail.com>
2025-12-29 21:27:07 +0530

    Add files via upload

---
+++ src/components/Post.jsx
@@ -0,0 +1,16 @@
+const Posts = ({ post }) => {
+  return (
+    <>
+      <div className="card post-card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">{post.title}</h5>
+          <p className="card-text">{post.body}</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Posts;
